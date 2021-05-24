baa1b2f261cb5c364f1a778c6765ed59c35028ba
yvan99 <ishimweyvan90@gmail.com>
2021-05-24 13:40:41 +0200

    includes branch added to git

---
+++ inc/js.php
@@ -0,0 +1,5 @@
+<script src="assets/homepage/js/jquery.min.js"></script>
+<script src="assets/homepage/js/bootstrap.min.js"></script>
+<script src="assets/homepage/js/jquery.easing.min.js"></script>
+<script src="assets/homepage/js/lity.min.js"></script>
+<script src="assets/homepage/js/custom.js"></script>
